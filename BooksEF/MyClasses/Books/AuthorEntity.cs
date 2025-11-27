bbcd056edcf3c6c2238b77278b57745a39259cc0
Андрей Чацкий <andrey-chatsky@yandex.ru>
2025-11-27 21:36:17 +0300

    Тест передачи

--- BooksEF/MyClasses/Books/AuthorEntity.cs
+++ BooksEF/MyClasses/Books/AuthorEntity.cs
@@ -2,9 +2,11 @@
 {
     public class AuthorEntity
     {
+        //Отправить на GitHub
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string SecondName { get; set; } 
         public string LastName { get; set; }
+        public string Email { get; set; }  
     }
 }
